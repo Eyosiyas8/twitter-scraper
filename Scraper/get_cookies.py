6e6ba259d7d524d440e986df5c8314bd4b43e08d
eyosiyas8 <eyosiyas8@gmial.com>
2023-06-20 19:41:08 +0300

    try reconnecting if there is no connection the first time

--- Scraper/get_cookies.py
+++ Scraper/get_cookies.py
@@ -5,6 +5,9 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.options import Options
 import os
 import pickle
+import urllib.request
+import urllib.error
+from log import error_log
 
 
 from selenium.webdriver.common.keys import Keys
@@ -14,31 +17,37 @@ import time
 import chromedriver_autoinstaller
 from selenium.common.exceptions import NoSuchElementException
 # Assigning a link to a webdriver
+for i in range(3):
+    try:
+        chromedriver_autoinstaller.install()
+        options = Options()
+        options.add_argument('--headless')
+        # chro_path = os.environ.get('CHROME_PATH')
+        driver = webdriver.Chrome(options=options)
 
-chromedriver_autoinstaller.install()
-options = Options()
-options.add_argument('--headless')
-# chro_path = os.environ.get('CHROME_PATH')
-driver = webdriver.Chrome(options=options)
+        driver.get("https://www.twitter.com/login")
+        print(driver.current_url)
 
-driver.get("https://www.twitter.com/login")
-print(driver.current_url)
+        print("Opening twitter account...")
 
-print("Opening twitter account...")
+        basedir = os.path.dirname(os.path.abspath(__file__))
+        # account_info = os.path.join(basedir, '../Authentication/Account.txt')
+        #log in into an account
 
-basedir = os.path.dirname(os.path.abspath(__file__))
-# account_info = os.path.join(basedir, '../Authentication/Account.txt')
-#log in into an account
-
-time.sleep(2)
-new_cookie = os.path.join(basedir, 'cookies.pkl')
-cookies = pickle.load(open(new_cookie, "rb"))
-def login():
-    for cookie in cookies:
-        try:
-            driver.add_cookie(cookie)
-        except Exception as e:
-            print(e)
-    # pickle.dump(cookies, open('cookies.pkl', "wb"))
-    driver.get("https://www.twitter.com/")
-    time.sleep(2)
+        time.sleep(2)
+        new_cookie = os.path.join(basedir, 'cookies.pkl')
+        cookies = pickle.load(open(new_cookie, "rb"))
+        def login():
+            for cookie in cookies:
+                try:
+                    driver.add_cookie(cookie)
+                except Exception as e:
+                    print(e)
+            # pickle.dump(cookies, open('cookies.pkl', "wb"))
+            driver.get("https://www.twitter.com/")
+            time.sleep(2)
+        break
+    except urllib.error.URLError as e:
+        time.sleep(20)
+        print("Connection Error: ", {e})
+        error_log(e)
