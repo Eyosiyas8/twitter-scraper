de73bdc17f09ed93bb8af4233ececd2d859dc67d
eyosiyas8 <eyosiyas8@gmial.com>
2024-04-15 14:01:24 +0300

    updated mongo cleaner

--- Scraper/filter_mongo_data.py
+++ Scraper/filter_mongo_data.py
@@ -7,9 +7,10 @@ collection = db["twitter"]
 
 # Define the filter to target documents with empty or missing "tweets" attribute
 filter = {"tweets": {"$exists": True, "$eq": []}}
+filter1 = {"UserName": {"$exists": True, "$in": ["@MapEthiopia", "@BarackObama", "@addisstandard", "@DanielBekele", "@Heyab_raya", "@MichelleObama", "@belfort_andrew", "@JoeBiden", "@taylorswift13", ""]}}
 
 # Delete documents matching the filter
-result = collection.delete_many(filter)
+result = collection.delete_many(filter1)
 
 # Output the number of deleted documents
 print("Deleted", result.deleted_count, "documents.")
